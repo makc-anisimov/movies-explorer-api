4fcc9684fe8878653567fa9472390139aefcd0be
Max Anisimov <makc.anisimov777@yandex.ru>
2023-07-20 12:04:47 +0300

    вынес rateLimiter в отдельный файл

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
-const rateLimit = require("express-rate-limit");
+const ratelimiter = require('./middlewares/ratelimiter');
 const index = require('./routes/index');
 
 require('dotenv').config();
@@ -13,15 +13,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
-const limiter = rateLimit({
-  max: 200,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many request from this IP"
-});
-
 const app = express();
 
-app.use(limiter);
+app.use(ratelimiter);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
